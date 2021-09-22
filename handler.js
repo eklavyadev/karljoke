b49f0127abb50c303c5793603f024b6a19334884
Eklavya <76553593+eklavyadev@users.noreply.github.com>
2021-09-22 21:21:07 +0530

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,32 @@
+const jokes = require('./source/jokes.json');
+
+const randomJoke = () => {
+  return jokes[Math.floor(Math.random() * jokes.length)];
+}
+
+/**
+ * Get N random jokes from a jokeArray
+ */
+const randomN = (jokeArray, n) => {
+  const limit = jokeArray.length < n ? jokeArray.length : n;
+  const randomIndicesSet = new Set();
+
+  while (randomIndicesSet.size < limit) {
+    const randomIndex = Math.floor(Math.random() * jokeArray.length);
+    if (!randomIndicesSet.has(randomIndex)) {
+      randomIndicesSet.add(randomIndex);
+    }
+  }
+
+  return Array.from(randomIndicesSet).map(randomIndex => {
+    return jokeArray[randomIndex];
+  });
+};
+
+const randomTen = () => randomN(jokes, 10);
+const randomNum = (num) => randomN(jokes, num)
+const jokeByType = (type, n) => {
+  return randomN(jokes.filter(joke => joke.type === type), n);
+};
+
+module.exports = { jokes, randomNum ,randomJoke, randomN, randomTen, jokeByType };
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+const LimitingMiddleware = require('limiting-middleware');
+const { randomJoke, randomTen, jokeByType, randomNum } = require('./handler');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.use(new LimitingMiddleware().limitByIp());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.post('/', (req, res) => {
+  res.send('Available Public Endpoints\n/jokes/random\n/jokes/ten\n/jokes/{id}\n/jokes/{type}/random\n/jokes/{type}/{number}\n/jokes/{type}/{id}\nWant to contribute? Just create a PR on blablabla.com 😀');
+});
+
+app.post('/jokes/random', (req, res) => {
+  res.json(randomJoke());
+});
+
+app.post('/jokes/ten', (req, res) => {
+  res.json(randomTen());
+});
+
+app.post('/jokes/:id', (req, res) => {
+    const number = parseInt(req.params.id)
+    res.json(randomNum(number));
+  });
+
+app.post('/jokes/:type/random', (req, res) => {
+  res.json(jokeByType(req.params.type, 1));
+});
+
+app.post('/jokes/:type/ten', (req, res) => {
+  res.json(jokeByType(req.params.type, 10));
+});
+
+app.post('/jokes/:type/:id', (req, res) => {
+    res.json(jokeByType(req.params.type, parseInt(req.params.id)));
+  });
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    type: 'error', message: err.message
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
